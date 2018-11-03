22ed232875304d1fda8c88a8b079444c2c676bd7
Владислав Люминарский <Vladislav.Lyuminarskiy@gmail.com>
2018-11-03 18:17:05 +0400

    Добавлено решение и тесты для задания 3.3.

---
+++ src/tests/test_task_3_3.py
@@ -0,0 +1,26 @@
+"""
+Тесты для задания 3.2.
+"""
+
+from unittest import TestCase
+import task_3_3
+
+
+class TestToDecreasingNumber(TestCase):
+
+    def test_to_decreasing_number__CorrectArguments__ShouldReturnCorrectResult(self):
+        """
+        Проверяет работу с корректными данными.
+        """
+
+        data = \
+            [( 71801,  87110),
+             (  1937,   9731),
+             (     0,      0),
+             ( -2038,  -8320),
+             (-71801, -87110)]
+
+        for decreasing_number, result in data:
+            with self.subTest():
+                self.assertEqual(task_3_3.to_decreasing_number(decreasing_number), result,
+                                 f"decreasing_number={decreasing_number}")
